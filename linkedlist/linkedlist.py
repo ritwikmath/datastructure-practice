c230dfd853a2c713b0f2887b2960b41263b490eb
Ritwik Math <ritwikmath@gmail.com>
2023-10-02 21:27:32 +0530

    Created a linked list
    Linkedlist class has addAtBeginning, addAtLast and printList methods

---
+++ linkedlist/linkedlist.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+@dataclass
+class Node:
+    value: str | int
+    next: None | Node = None
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head: None | Node = None
+    
+    def addAtBeginning(self, data) -> Node:
+        if (self.head is None):
+            self.head = Node(data)
+            return self.head
+        
+        node = Node(data)
+        node.next = self.head
+        self.head = node
+        return self.head
+    
+    def addAtLast(self, data) -> None:
+        if (self.head is None):
+            self.head = Node(data)
+        
+        current = self.head
+        while(current is not None):
+            if (current.next is None):
+                node = Node(data)
+                current.next = node
+                break
+            current = current.next
+    
+    def printList(self):
+        current = self.head
+        while(current is not None):
+            print(current.value)
+            current = current.next
+        print('Lined list traverse complete')
+
+l = LinkedList()
+l.addAtBeginning(4)
+l.addAtBeginning(3)
+l.addAtBeginning(2)
+l.addAtBeginning(1)
+l.addAtLast(5)
+l.printList()

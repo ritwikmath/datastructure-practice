443f531c7b0393c844d4bb79164cfc362939be3c
ritwikmath <38456727+ritwikmath@users.noreply.github.com>
2023-12-14 21:05:47 +0530

    Without OS package

--- string/longest-common-prefix.py
+++ string/longest-common-prefix.py
@@ -1,4 +1,15 @@
-import os
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        return os.path.commonprefix(strs)
+    def longestCommonPrefix(self, strs) -> str:
+        sorted_strs = sorted(strs)
+        key = 0
+        prefix = ''
+        while True:
+            try:
+                if sorted_strs[0][key] == sorted_strs[-1][key]:
+                    prefix += sorted_strs[0][key]
+                else:
+                    break
+            except IndexError:
+                break
+            key += 1
+        return prefix
